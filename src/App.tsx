d23f01be0c43b1a368ba7965b96b2a7c03c5ce6c
Maria Vitória Silva <mariavitoriasilvast@gmail.com>
2022-10-17 22:09:48 -0300

    feature: add component tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import { Header } from './components/Header/index';
+import { Tasks } from './components/Tasks';
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Tasks />
+    </>
+  );
 }
 
 export default App;
